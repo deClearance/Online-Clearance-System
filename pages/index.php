029044c09a7eca21d57bb2377e6259f3e0706230
Biruk <biruk.alamirew77@gmail.com>
2021-08-26 01:06:15 -0700

    moved the navbar to a separate php file

--- pages/index.php
+++ pages/index.php
@@ -63,57 +63,13 @@ if (!isset($_SESSION['id'])) {
                     </button>
                     <a class="navbar-brand" href="index.html">You are <?php echo $_SESSION['user_role'] ?></a>
                 </div>
-                <div style="color: white;
-padding: 15px 50px 5px 50px;
-float: right;
-font-size: 16px;">
-                    <a style="margin-right: 25px;"><?php echo $_SESSION['office'] . '   '  ?> </a> <a href="./logout.php" class="btn btn-danger square-btn-adjust">Logout</a>
+                <div style="color: white; padding: 15px 50px 5px 50px; float: right; font-size: 16px;">
+                    <a style="margin-right: 25px;"><?php echo $_SESSION['office'] . '   '?> </a> <a href="./logout.php" class="btn btn-danger square-btn-adjust">Logout</a>
                 </div>
             </nav>
-            <!-- /. NAV TOP  -->
-            <nav class="navbar-default navbar-side" role="navigation">
-                <div class="sidebar-collapse">
-                    <ul class="nav" id="main-menu">
-                        <li class="text-center">
-                            <img src="../img/find_user.png" class="user-image img-responsive" />
-                        </li>
-
-
-                        <li>
-                            <a class="active-menu" href="./index.php"><i class="fa fa-dashboard fa-3x"></i>Home</a>
-                        </li>
-                        <!-- Admins Only -->
-                        <?php if($_SESSION['role'] ==3 || $_SESSION['role'] == 2) {?>
-
-                        <li>
-                            <a href="./addClerance.php"><i class="fa fa-desktop fa-3x"></i>Add Clearances</a>
-                        </li>
-                        <li>
-                            <a href="./index.php#remaining-clearance"><i class="fa fa-bar-chart-o fa-3x"></i> View Clearance Details</a>
-                        </li>
-                        <li>
-                            <a href="./index.php"><i class="fa fa-table fa-3x"></i>Update Clearances</a>
-                        </li>
-                        
-                        <li>
-                            <a href="./materials.php"><i class="fa fa-square-o fa-3x"></i> Available Materials</a>
-                        </li><?php }?>
-                        <!-- Admins Only -->
-
-                        <li>
-                            <a href="./sendFeedback.php"><i class="fa fa-qrcode fa-3x"></i>sendFeedback</a>
-                        </li>
-                       
-                      
-
-
-
-                    </ul>
-
-                </div>
-
-            </nav>
-            <!-- /. NAV SIDE  -->
+            
+            <?php include './navbar.php'; ?>
+            
             <div id="page-wrapper">
                 <div id="page-inner">
                     <div class="row">
